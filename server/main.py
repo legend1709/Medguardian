45ffba4c16718a1b63393206376b39c89248415d
Legend1709 <sy4710165@gmail.com>
2026-09-17 21:02:43 +0530

    Fix Vercel CORS

--- server/main.py
+++ server/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https:// medguardian-o1w9.vercel.app",  # apna Vercel URL
+        "https://medguardian-o1w9.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
