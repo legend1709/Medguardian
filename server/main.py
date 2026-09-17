3863cf3d965c504131d5e3a5ea0972c780126cf9
Legend1709 <sy4710165@gmail.com>
2026-09-17 19:14:23 +0530

    Add Vercel SPA rewrite

--- server/main.py
+++ server/main.py
@@ -17,12 +17,13 @@ app = FastAPI(
 # Database initialize
 init_database()
 
-# CORS (React Frontend)
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https:// medguardian-o1w9.vercel.app",  # apna Vercel URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
