ebb5ef406d159251c785f8f5ae4ad926838e8969
Legend1709 <sy4710165@gmail.com>
2026-09-17 19:04:20 +0530

    Fix Google OAuth redirect

--- client/src/supabase/supabase.js
+++ client/src/supabase/supabase.js
@@ -3,7 +3,13 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://cadcjaxmulrpdxkintsw.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhZGNqYXhtdWxycGR4a2ludHN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2MTYxNzIsImV4cCI6MjEwNTE5MjE3Mn0.1PTkkPkYhkW3RKLIVcsw8P49ApSwuSmXWM29_YzTpp8";
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const signInWithGoogle = async () => {
+  return supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: `${window.location.origin}/home`,
+    },
+  });
+};
